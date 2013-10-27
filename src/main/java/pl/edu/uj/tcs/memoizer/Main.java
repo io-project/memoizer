7aeb7ef5573d841dfaacf8f35084b00184d7b970
Ziemin <brentprey@gmail.com>
2013-10-27 14:56:35 +0100

    Added main function and defined it in pom.xml in order to make generated jar executable

---
+++ src/main/java/pl/edu/uj/tcs/memoizer/Main.java
@@ -0,0 +1,8 @@
+package pl.edu.uj.tcs.memoizer;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("The main function of our superior app");
+    }
+}
