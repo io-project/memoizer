43f8298242295a7a11ff7d057b9de04e1083e973
sokar92 <patryk912@gmail.com>
2013-11-11 14:02:26 +0100

    DemotyPlugin implementation using meme buffers

--- src/main/java/pl/edu/uj/tcs/memoizer/plugins/impl/demoty/DemotyDownloadPlugin.java
+++ src/main/java/pl/edu/uj/tcs/memoizer/plugins/impl/demoty/DemotyDownloadPlugin.java
@@ -1,24 +1,35 @@
 package pl.edu.uj.tcs.memoizer.plugins.impl.demoty;
 
 import pl.edu.uj.tcs.memoizer.plugins.*;
+import pl.edu.uj.tcs.memoizer.plugins.impl.*;
 
 import java.net.*;
 import java.util.*;
+import java.lang.IllegalArgumentException;
 import java.awt.Image;
 
 /*
  * Plugin designed to downloading from Demotywatory page
  * @author pmikos (sokar92)
  */
-public class DemotyDownloadPlugin implements IDownloadPlugin {
+public final class DemotyDownloadPlugin implements IDownloadPlugin {
 	private DemotyPropertiesSet _properties;
+	private EViewType _view;
+	private Map<EViewType, IMemeBuffer> _viewBuffer;
 	
 	/*
 	 * Instantiates new download plugin designed
 	 * for "Demotywatory.pl" page
 	 */
-	public DemotyDownloadPlugin(){
+	public DemotyDownloadPlugin(){		
 		_properties = new DemotyPropertiesSet();
+		_view = null;
+		
+		_viewBuffer = new HashMap<EViewType, IMemeBuffer>();
+		_viewBuffer.put(EViewType.CHRONOLOGICAL, new DemotyMemeBufferChrono());
+		_viewBuffer.put(EViewType.UNSEEN, new DemotyMemeBufferUnseen());
+		_viewBuffer.put(EViewType.FAVOURITE, new DemotyMemeBufferTop());
+		_viewBuffer.put(EViewType.QUEUE, new DemotyMemeBufferQueue());
 	}
 	
 	@Override
@@ -42,25 +53,41 @@ public class DemotyDownloadPlugin implements IDownloadPlugin {
 
 	@Override
 	public List<EViewType> getAvailableViews() {
-		return null;
+		List<EViewType> list = new ArrayList<EViewType>();
+		list.add(EViewType.CHRONOLOGICAL);
+		list.add(EViewType.FAVOURITE);
+		list.add(EViewType.UNSEEN);
+		list.add(EViewType.QUEUE);
+		return list;
 	}
 
 	@Override
 	public void setView(EViewType viewType) {
+		if(!getAvailableViews().contains(viewType))
+			throw new IllegalArgumentException(
+					"Demoty plugin does not support " + viewType.toString() + " view!");
+		
+		_view = viewType;
 	}
 
 	@Override
 	public boolean hasNext(){
-		return true;
+		if(_view == null)
+			return false;
+		return _viewBuffer.get(_view).hasNext();
 	}
 	
 	@Override
 	public Meme getNext(){
-		return null;
+		if(_view == null)
+			return null;
+		return _viewBuffer.get(_view).getNext();
 	}
 	
 	@Override
 	public Iterable<Meme> getNext(int n){
-		return null;
+		if(_view == null)
+			return null;
+		return _viewBuffer.get(_view).getNext(n);
 	}
 }
--- src/main/java/pl/edu/uj/tcs/memoizer/plugins/impl/demoty/DemotyMemeBufferUnseen.java
+++ src/main/java/pl/edu/uj/tcs/memoizer/plugins/impl/demoty/DemotyMemeBufferUnseen.java
@@ -1,6 +1,7 @@
-package pl.edu.uj.tcs.memoizer.plugins.impl;
+package pl.edu.uj.tcs.memoizer.plugins.impl.demoty;
 
 import pl.edu.uj.tcs.memoizer.plugins.Meme;
+import pl.edu.uj.tcs.memoizer.plugins.impl.MemeBuffer;
 
 public class DemotyMemeBufferUnseen extends MemeBuffer {
 	
