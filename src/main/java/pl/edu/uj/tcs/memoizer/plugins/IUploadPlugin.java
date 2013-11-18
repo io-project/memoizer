f4b9bb5cdec815e7ce00e0949767b4cb511961d8
Ziemin <brentprey@gmail.com>
2013-11-18 22:48:36 +0100

    Removed interfaces related to development of plugins. They are added as dependency in pom.xml

--- src/main/java/pl/edu/uj/tcs/memoizer/plugins/IUploadPlugin.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.uj.tcs.memoizer.plugins;
-
-/*
- * Interface implemented by any Upload plugin
- * @author pmikos (sokar92)
- */
-public interface IUploadPlugin extends IPlugin {
-	
-}
