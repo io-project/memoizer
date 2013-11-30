373d257c7ae8ac17c4e8ed5ae5683a6230c64866
Ziemin <brentprey@gmail.com>
2013-11-30 22:32:50 +0100

    Added final to string

--- src/main/java/pl/edu/uj/tcs/memoizer/configuration/Version.java
+++ src/main/java/pl/edu/uj/tcs/memoizer/configuration/Version.java
@@ -1,4 +1,4 @@
 package pl.edu.uj.tcs.memoizer.configuration;
 public final class Version {
- public static String VERSION="0.0.2-SNAPSHOT";
+  public static final String VERSION="0.0.2-SNAPSHOT";
 }
