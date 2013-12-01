70737832260e9413d00d1cc004c0c4c00d7f8428
Ziemin <brentprey@gmail.com>
2013-12-01 23:21:50 +0100

    Fixed tests to work with changed constructors

--- src/test/java/pl/edu/uj/tcs/memoizer/plugins/communication/mocks/EmptyDownloadPluginMock.java
+++ src/test/java/pl/edu/uj/tcs/memoizer/plugins/communication/mocks/EmptyDownloadPluginMock.java
@@ -1,12 +1,13 @@
 package pl.edu.uj.tcs.memoizer.plugins.communication.mocks;
 
 import pl.edu.uj.tcs.memoizer.plugins.EViewType;
+import pl.edu.uj.tcs.memoizer.plugins.IPluginFactory;
 import pl.edu.uj.tcs.memoizer.plugins.Meme;
 
 public class EmptyDownloadPluginMock extends DownloadPluginMock {
 
-	public EmptyDownloadPluginMock(String name, EViewType viewType) {
-		super(name, viewType);
+	public EmptyDownloadPluginMock(String name, EViewType viewType, IPluginFactory parent) {
+		super(name, viewType, parent);
 		// TODO Auto-generated constructor stub
 	}
 	
